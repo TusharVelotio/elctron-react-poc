907c4df0e3bba550f5ee24138f882b3922e4d4ee
Tushar Tibude <tushar.tibude@velotio.com>
2024-10-28 15:55:24 +0530

    Websocket added

--- electron-react-boilerplate-main/src/renderer/App.tsx
+++ electron-react-boilerplate-main/src/renderer/App.tsx
@@ -1,14 +1,22 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import InputComponent from './input';
+import WS from '../websocket/ws';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Electron app
-          <InputComponent/>
-        </div>} />
+        <Route
+          path="/"
+          element={
+            <div>
+              Electron app
+              <InputComponent />
+              <WS />
+            </div>
+          }
+        />
       </Routes>
     </Router>
   );
